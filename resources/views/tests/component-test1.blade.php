eae203fbe38baea7823f0e62f39037970a0a797a
syooooring <syooooring@gmail.com>
2021-09-09 01:16:47 +0900

    [update]属性バック

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -5,4 +5,5 @@
 コンポーネントテスト1
   <x-tests.card title="タイトル1" content="本文1" :message="$message" />
   <x-tests.card title="タイトル123" />
+  <x-tests.card title="CSSを変更した" class="bg-red-300" />
 </x-tests.app>

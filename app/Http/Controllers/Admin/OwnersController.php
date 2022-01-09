c5a765a07f1e25bfb2e010210bcc1045860deaaa
syooooring <syooooring@gmail.com>
2022-01-09 19:33:56 +0900

    softDelete

--- app/Http/Controllers/Admin/OwnersController.php
+++ app/Http/Controllers/Admin/OwnersController.php
@@ -62,7 +62,8 @@ class OwnersController extends Controller
         ]);
 
         return redirect()->route('admin.owners.index')
-        ->with('message', 'オーナーを登録しました。');
+        ->with(['message' => 'オーナー登録しました。',
+        'status' => 'info']);
 
     }
 
@@ -105,7 +106,8 @@ class OwnersController extends Controller
         $owner->save;
 
         return redirect()->route('admin.owners.index')
-        ->with('message', 'オーナー情報を更新しました。');
+        ->with(['message' => 'オーナー情報を更新しました。',
+        'status' => 'info']);
     }
 
     /**
@@ -116,6 +118,24 @@ class OwnersController extends Controller
      */
     public function destroy($id)
     {
-        //
+        Owner::findOrFail($id)->delete();
+
+        return redirect()->route('admin.owners.index')
+        ->with(['message' => 'オーナー情報を削除しました。',
+        'status' => 'alert']);
+    }
+
+    public function expiredOwnerIndex()
+    {
+        $expiredOwners = Owner::onlyTrashed()->get();
+
+        return view('admin.expired-owners', compact('expiredOwners'));
+    }
+
+    public function expiredOwnerDestroy($id)
+    {
+        Owner::onlyTrashed()->findOrFail($id)->forceDelete();
+
+        return redirect()->route('admin.expired-owners.index');
     }
 }
